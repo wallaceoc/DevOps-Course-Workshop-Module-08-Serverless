81052ab560dc709af259b71043055a0214939353
Luke Storry <Luke@storry.uk>
2021-03-08 13:44:09 +0000

    added comment explaining sleep

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def day():
-    sleep(2)
+    sleep(2) # Simulating 2 seconds of cpu-intensive processing
     return "Today is " + datetime.datetime.now().strftime("%A")
