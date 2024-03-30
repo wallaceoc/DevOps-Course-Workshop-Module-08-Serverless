8dc5f2c9bd61c042ad176c163efd2089ff3aa9ca
Billy O'Connell <billy.oconnell@gmail.com>
2024-03-30 14:52:41 +0000

    Commit module 8 course work

--- AcmeSubProject/TranslationQueueTrigger/__init__.py
+++ AcmeSubProject/TranslationQueueTrigger/__init__.py
@@ -1,8 +1,10 @@
 import logging
+import json
 
 from azure.functions import QueueMessage
 
 
-def main(msg: QueueMessage) -> None:
+def main(msg: QueueMessage, translationTable) -> None:
     logging.info('Python queue trigger function processed a queue item: %s',
                  msg.get_body().decode('utf-8'))
+    message = json.loads(translationTable)
